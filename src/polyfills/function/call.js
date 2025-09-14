e10528a77eee95f56a940ec971bd32d49ab8872b
abhinavoo7 <gabhinav95a@gmail.com>
2025-09-14 23:32:48 +0530

    added js doc to custom call

--- src/polyfills/function/call.js
+++ src/polyfills/function/call.js
@@ -1,3 +1,11 @@
+/**
+ * @function customCall
+ * @description polyfill for Function.prototype.call. Invokes the function with a specified this context and arguments.
+ * @param {unknown} [thisArg] value to use as this when calling the function.
+ * @param  {...unknown} argArray arguments to pass to the function
+ * @returns {unknown} result of invoking the function with the specified context and arguments.
+ * @throws {TypeError} if customCall is invoked on a non-function
+ */
 Function.prototype.customCall = function (thisArg, ...argArray) {
   if (typeof this !== "function") {
     throw new TypeError("customCall must be called on a function");
